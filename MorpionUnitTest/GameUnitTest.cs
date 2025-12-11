c7ce9c3b022d902f69835a5cacbe3f7f857d0b56
CSTLLI <valeriocastellipro@gmail.com>
2025-12-11 16:49:20 +0100

    feat: init UnitTest

---
+++ MorpionUnitTest/GameUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Morpion;
+
+namespace MorpionUnitTest;
+
+public class GameUnitTest
+{
+    [Fact]
+    public void RunGameTest()
+    {
+        var game = new Game();
+
+        try
+        {
+            game.Run();
+        }
+        catch (InvalidOperationException)
+        {
+
+        }
+        finally
+        {
+            Assert.True(true);
+        }
+    }
+}
